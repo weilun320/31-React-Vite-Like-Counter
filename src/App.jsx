5f118ae87d2ebcd22030dfd04d1cfb341c585a52
Wei Lun <weilun9320@hotmail.com>
2023-12-11 16:42:19 +0800

    Added Like & Dislike button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useState } from "react"
+import { Button } from "react-bootstrap";
 
 function App() {
-  return (
-    <>
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
 
-    </>
+  function increaseLike() {
+    setLikeCount(likeCount + 1);
+  }
+
+  function increaseDislike() {
+    setDislikeCount(dislikeCount + 1);
+  }
+
+  return (
+    <div className="m-3 text-center">
+      <Button variant="success" className="me-1" onClick={increaseLike}>Like {likeCount}</Button>
+      <Button variant="danger" className="ms-1" onClick={increaseDislike}>Dislike {dislikeCount}</Button>
+    </div>
   )
 }
 
